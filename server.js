dceb6a0a826acd3eac346abfd90119bc84b4f141
Denzale <denzalethouston@gmail.com>
2021-09-11 18:00:04 -0700

    slice colors

--- server.js
+++ server.js
@@ -1,11 +1,63 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/", function(req, res){
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//data json
+//-----------------------------------------
+const homies = [{
+    routeName: "yoda",
+    name: "yoda",
+    age: 500
+},
+{
+    routeName: "denzale",
+    name: "denzale",
+    age: 23
+}]
+
+
+
+
+
+//Routes
+//--------------------------------------
+app.get("/", function (req, res) {
     res.send("My Home Page!")
 });
 
+app.get("/api/homies", function (req, res) {
+    res.json(homies)
+});
+
+let myShirts = [
+    "Red",
+    "Blue",
+    "Yellow"
+]
+
+arraySwap = myShirts.slice(2)
+
+
+
+//console.log(arraySwap)
+
+function whatToWear() {
+    let color = "I am going to wear"(arraySwap) 
+    
+console.log (color)
+}
+
+if(whatToWear = "yellow") {
+console.log ("Thats a good color!")
+
+}
+//listener 
+//--------------------------------------
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
-  })
+})
